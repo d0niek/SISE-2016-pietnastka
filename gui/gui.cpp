b49a134339072df9bf1759a3b45b7f614e65dfa3
d0niek <damianglinkowski@gmail.com>
2016-06-03 16:07:55 +0200

    Rysowanie piętnastki przez wxPanel

--- gui/gui.cpp
+++ gui/gui.cpp
@@ -10,6 +10,7 @@
 #include <wx/string.h>
 #include <wx/button.h>
 #include <wx/radiobox.h>
+#include "wx/sizer.h"
 
 #include <iostream>
 
@@ -17,47 +18,63 @@ using namespace std;
 
 #endif
 
+class BasicDrawPanel : public wxPanel
+{
+    wxString fifteen[16];
+    int emptyElement;
+    wxSize fifteenSize;
+    wxString solution;
+
+    void DrawFifteen(wxDC &dc);
+
+    void DrawGrid(wxDC &dc);
+
+    void FillGrid(wxDC &dc);
+
+    void SlideElement(wxUniChar direction);
+
+public:
+    BasicDrawPanel(wxFrame *parent);
+
+    void paintEvent(wxPaintEvent &evt);
+
+    void paintNow();
+
+    void render(wxDC &dc);
+
+    void Solve(const wxString &solution);
+
+    void SetFifteen(wxString fifteenElements[], int emptyElement, const wxSize &fifteenSize);
+
+    void SetSolution(const wxString &solution);
+
+DECLARE_EVENT_TABLE()
+};
+
+class MyFrame;
+
 class MyApp : public wxApp
 {
+    BasicDrawPanel *drawPanel;
+    bool render_loop_on;
+
+    bool OnInit();
+
+    void onIdle(wxIdleEvent &evt);
+
 public:
-    virtual bool OnInit();
+    void activateRenderLoop(bool on);
 };
 
 class MyFrame : public wxFrame
 {
-public:
-    MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
-
-    ~MyFrame();
-
-    void Init();
-
-private:
-    wxClientDC *dc = nullptr;
-
+    BasicDrawPanel *drawPanel;
     wxString openedFile;
-
-    wxSize fifteenSize;
-
-    wxButton *(fifteenElements[16]);
-
-    int emptyElementPos;
-
     wxRadioBox *solutions = nullptr;
-
     wxButton *solveBtn = nullptr;
+    bool solved;
 
-    void AddMenu();
-
-    void OpenFile(wxCommandEvent &event);
-
-    void ReadFileContent(wxFile *file);
-
-    void DrawFifteen(wxString fifteen);
-
-    void EraseFifteen();
-
-    void DrawSolutionsOptions();
+    void OnOpen(wxCommandEvent &event);
 
     void OnExit(wxCommandEvent &event);
 
@@ -65,11 +82,22 @@ private:
 
     void OnSolve(wxCommandEvent &event);
 
-    void Solve(wxFile *file);
+    void AddMenu();
 
-    void SlideElement(wxUniChar direction);
+    void ReadFileContent(wxFile *file);
 
-    void DrawTextString(const wxString &text, const wxPoint &pt);
+    void ReadFifteenAndSendItToDrawPanel(wxString fifteen);
+
+    void DrawSolutionsOptions();
+
+    wxString LoadSolution();
+
+public:
+    MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+    ~MyFrame();
+
+    void SetDrawPanel(BasicDrawPanel *drawPanel);
 
 wxDECLARE_EVENT_TABLE();
 };
@@ -83,7 +111,7 @@ enum
 
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
 
-                EVT_MENU(ID_Load_file, MyFrame::OpenFile)
+                EVT_MENU(ID_Load_file, MyFrame::OnOpen)
                 EVT_MENU(wxID_EXIT, MyFrame::OnExit)
                 EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
                 EVT_BUTTON(ID_SOLVE, MyFrame::OnSolve)
@@ -94,21 +122,161 @@ wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit()
 {
+    render_loop_on = false;
+
     MyFrame *frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
-    frame->Show(true);
+    drawPanel = new BasicDrawPanel(frame);
 
-    frame->Init();
+    wxBoxSizer *sizer = new wxBoxSizer(wxHORIZONTAL);
+    sizer->Add(drawPanel);
 
+    frame->SetSizer(sizer);
+    frame->SetDrawPanel(drawPanel);
+    frame->Show();
+
+    activateRenderLoop(true);
     return true;
+}
+
+void MyApp::activateRenderLoop(bool on)
+{
+    if (on && !render_loop_on) {
+        Connect(wxID_ANY, wxEVT_IDLE, wxIdleEventHandler(MyApp::onIdle));
+        render_loop_on = true;
+    } else if (!on && render_loop_on) {
+        Disconnect(wxEVT_IDLE, wxIdleEventHandler(MyApp::onIdle));
+        render_loop_on = false;
+    }
+}
+
+void MyApp::onIdle(wxIdleEvent &evt)
+{
+    if (render_loop_on) {
+        drawPanel->paintNow();
+        evt.RequestMore();
+    }
+}
+
+BEGIN_EVENT_TABLE(BasicDrawPanel, wxPanel)
+
+                EVT_PAINT(BasicDrawPanel::paintEvent)
+
+END_EVENT_TABLE()
+
+BasicDrawPanel::BasicDrawPanel(wxFrame *parent) :
+        wxPanel(parent, wxID_ANY, wxPoint(30, 30), wxSize(120, 120))
+{
+    this->emptyElement = -1;
+}
+
+void BasicDrawPanel::paintEvent(wxPaintEvent &evt)
+{
+    wxPaintDC dc(this);
+    render(dc);
+}
+
+void BasicDrawPanel::paintNow()
+{
+    wxClientDC dc(this);
+    render(dc);
+}
+
+void BasicDrawPanel::render(wxDC &dc)
+{
+    dc.SetBackground(*wxLIGHT_GREY_BRUSH);
+    dc.Clear();
+
+    if (!this->solution.empty()) {
+
+    }
+
+    if (this->emptyElement >= 0) {
+        this->DrawFifteen(dc);
+    }
+}
+
+void BasicDrawPanel::DrawFifteen(wxDC &dc)
+{
+    this->DrawGrid(dc);
+    this->FillGrid(dc);
+}
+
+void BasicDrawPanel::DrawGrid(wxDC &dc)
+{
+    for (int i = 0; i < this->fifteenSize.x - 1; i++) {
+        int y = 30 + (30 * i);
+        dc.DrawLine(wxPoint(0, y), wxPoint(30 * this->fifteenSize.y, y));
+    }
+
+    for (int i = 0; i < this->fifteenSize.y - 1; i++) {
+        int x = 30 + (30 * i);
+        dc.DrawLine(wxPoint(x, 0), wxPoint(x, 30 * this->fifteenSize.x));
+    }
+}
+
+void BasicDrawPanel::FillGrid(wxDC &dc)
+{
+    int size = this->fifteenSize.x * this->fifteenSize.y;
+
+    for (int i = 0; i < size; i++) {
+        if (this->fifteen[i].compare("0") != 0) {
+            int x = 7 + 30 * (i % this->fifteenSize.y);
+            int y = 7 + 30 * (i / this->fifteenSize.y);
+            dc.DrawText(this->fifteen[i], wxPoint(x, y));
+        }
+    }
+}
+
+void BasicDrawPanel::Solve(const wxString &solution)
+{
+    // Skip solution size
+    int endlPos = solution.find("\n");
+    wxString solutionPath = solution.substr(endlPos + 1);
+
+    for (int i = 0; i < solutionPath.length(); i++) {
+        cout << i << "<-\n";
+        this->SlideElement(solutionPath[i]);
+
+        wxMilliSleep(300);
+    }
+}
+
+void BasicDrawPanel::SlideElement(wxUniChar direction)
+{
+//    int element;
+//
+//    if (direction == 'l') {
+//        element = this->emptyElementPos - 1;
+//    } else if (direction == 'u') {
+//        element = this->emptyElementPos - this->fifteenSize.GetY();
+//    } else if (direction == 'r') {
+//        element = this->emptyElementPos + 1;
+//    } else if (direction == 'd') {
+//        element = this->emptyElementPos + this->fifteenSize.GetY();
+//    }
+//
+//    this->fifteenElements[this->emptyElementPos] = this->fifteenElements[element];
+//    this->emptyElementPos = element;
+//    this->fifteenElements[this->emptyElementPos] = "0";
+}
+
+void BasicDrawPanel::SetFifteen(wxString fifteen[], int emptyElement, const wxSize &fifteenSize)
+{
+    for (int i = 0; i < 16; i++) {
+        this->fifteen[i] = fifteen[i];
+    }
+    this->emptyElement = emptyElement;
+    this->fifteenSize = fifteenSize;
+}
+
+void BasicDrawPanel::SetSolution(const wxString &solution)
+{
+    this->solution = solution;
 }
 
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
         : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-    for (int i = 0; i < 16; i++) {
-        this->fifteenElements[i] = nullptr;
-    }
-
     this->AddMenu();
 
     CreateStatusBar();
@@ -117,14 +285,8 @@ MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
 
 MyFrame::~MyFrame()
 {
-    delete this->dc;
     delete this->solutions;
     delete this->solveBtn;
-}
-
-void MyFrame::Init()
-{
-    this->dc = new wxClientDC(this);
 }
 
 void MyFrame::AddMenu()
@@ -143,7 +305,7 @@ void MyFrame::AddMenu()
     SetMenuBar(menuBar);
 }
 
-void MyFrame::OpenFile(wxCommandEvent &event)
+void MyFrame::OnOpen(wxCommandEvent &event)
 {
     wxFileDialog fileDialog(
             this,
@@ -162,6 +324,8 @@ void MyFrame::OpenFile(wxCommandEvent &event)
 
         if (inputStream.IsOk()) {
             this->ReadFileContent(inputStream.GetFile());
+
+            this->solved = false;
         } else {
             wxLogError("Cannot open file '%s'.", fileDialog.GetPath());
         }
@@ -173,54 +337,38 @@ void MyFrame::ReadFileContent(wxFile *file)
     wxString fileContent;
     file->ReadAll(&fileContent);
 
+    this->ReadFifteenAndSendItToDrawPanel(fileContent);
+    this->DrawSolutionsOptions();
+}
+
+void MyFrame::ReadFifteenAndSendItToDrawPanel(wxString fileContent)
+{
+    wxString fifteenElements[16];
+    int emptyElement = -1;
+
     int x = wxAtoi(fileContent[0]);
     int y = wxAtoi(fileContent[2]);
-
-    this->fifteenSize.Set(x, y);
 
     wxString fifteen = fileContent.substr(4);
     fifteen.Replace("\n", " ");
 
-    this->DrawFifteen(fifteen);
-    this->DrawSolutionsOptions();
-}
-
-void MyFrame::DrawFifteen(wxString fifteen)
-{
-    this->EraseFifteen();
-
     int startPos = 0;
     int endPos;
 
-    for (int i = 0; i < this->fifteenSize.GetX() * this->fifteenSize.GetY(); i++) {
+    for (int i = 0; i < x * y; i++) {
         endPos = fifteen.find(' ', startPos);
 
         wxString fieldValue = fifteen.substr(startPos, endPos - startPos);
+        fifteenElements[i] = fieldValue;
 
-        if (fieldValue.compare("0") != 0) {
-            wxButton *element = new wxButton(
-                    this,
-                    wxID_ANY,
-                    fieldValue,
-                    wxPoint(30 * (i % this->fifteenSize.GetY()), 30 * (i / this->fifteenSize.GetY())),
-                    wxSize(30, 30)
-            );
-
-            this->fifteenElements[i] = element;
-        } else{
-            this->emptyElementPos = i;
+        if (fieldValue.compare("0") == 0) {
+            emptyElement = i;
         }
 
         startPos = endPos + 1;
     }
-}
 
-void MyFrame::EraseFifteen()
-{
-    for (int i = 0; i < 16; i++) {
-        delete this->fifteenElements[i];
-        this->fifteenElements[i] = nullptr;
-    }
+    this->drawPanel->SetFifteen(fifteenElements, emptyElement, wxSize(x, y));
 }
 
 void MyFrame::DrawSolutionsOptions()
@@ -249,6 +397,7 @@ void MyFrame::DrawSolutionsOptions()
 
 void MyFrame::OnExit(wxCommandEvent &event)
 {
+    wxGetApp().activateRenderLoop(false);
     Close(true);
 }
 
@@ -263,68 +412,35 @@ void MyFrame::OnAbout(wxCommandEvent &event)
 
 void MyFrame::OnSolve(wxCommandEvent &event)
 {
-    wxString option = this->solutions->GetStringSelection();
-    wxString solutionFile("../out/");
+    if (!this->solved) {
+        wxString solution = this->LoadSolution();
+        this->drawPanel->SetSolution(solution);
 
+        this->solved = true;
+    }
+}
+
+wxString MyFrame::LoadSolution()
+{
+    wxString option = this->solutions->GetStringSelection();
+
+    wxString solutionFile("../out/");
     solutionFile << this->openedFile << "_" << option.Lower() << ".out";
 
     wxFileInputStream inputStream(solutionFile);
 
     if (inputStream.IsOk()) {
-        this->Solve(inputStream.GetFile());
+        wxFile *file = inputStream.GetFile();
+        wxString solution;
+        file->ReadAll(&solution);
+
+        return solution;
     } else {
         wxLogError("Cannot open file '%s'.", solutionFile);
     }
 }
 
-void MyFrame::Solve(wxFile *file)
+void MyFrame::SetDrawPanel(BasicDrawPanel *drawPanel)
 {
-    wxString fileContent;
-    file->ReadAll(&fileContent);
-
-    // Skip solution size
-    int endlPos = fileContent.find("\n");
-    wxString solution = fileContent.substr(endlPos + 1);
-
-    for (int i = 0; i < solution.length(); i++) {
-        this->SlideElement(solution[i]);
-    }
-}
-
-void MyFrame::SlideElement(wxUniChar direction)
-{
-    int element;
-    int x = 0, y = 0;
-
-    if (direction == 'l') {
-        element = this->emptyElementPos - 1;
-        x = 30;
-    } else if (direction == 'u') {
-        element = this->emptyElementPos - this->fifteenSize.GetY();
-        y = 30;
-    } else if (direction == 'r') {
-        element = this->emptyElementPos + 1;
-        x = -30;
-    } else if (direction == 'd') {
-        element = this->emptyElementPos + this->fifteenSize.GetY();
-        y = -30;
-    }
-
-    wxPoint elementPos = this->fifteenElements[element]->GetPosition();
-    this->fifteenElements[element]->Move(elementPos.x + x, elementPos.y + y);
-
-    this->fifteenElements[this->emptyElementPos] = this->fifteenElements[element];
-    this->fifteenElements[element] = nullptr;
-
-    this->emptyElementPos = element;
-}
-
-void MyFrame::DrawTextString(const wxString &text, const wxPoint &pt)
-{
-    wxFont font(12, wxFONTFAMILY_SWISS, wxNORMAL, wxBOLD);
-    this->dc->SetFont(font);
-    this->dc->SetBackgroundMode(wxTRANSPARENT);
-    this->dc->SetTextForeground(*wxBLACK);
-    this->dc->SetTextBackground(*wxWHITE);
-    this->dc->DrawText(text, pt);
+    this->drawPanel = drawPanel;
 }
